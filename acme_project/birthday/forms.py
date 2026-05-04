dc3f188ceb4433b58dd53ae4d8ccf7c9efa6db6a
PavelMilyutikov <sevastopolrem@yandex.ru>
2026-05-04 23:17:20 +0300

    04.05

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+
+class BirthdayForm(forms.Form):
+    first_name = forms.CharField(
+        max_length=20,
+        label='Имя'
+    )
+    last_name = forms.CharField(
+        required=False,
+        label='Фамилия',
+        help_text='Необязательное поле'
+    )
+    birthday = forms.DateField(
+        label='Дата рождения'
+    )
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from birthday.forms import BirthdayForm
+
 
 def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+
+    form = BirthdayForm()
+    context = {'form': form}
+    return render(request, 'birthday/birthday.html', context)
